fd20e2ce6616583b71e4e685455b5f877b26bbab
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-04-24 19:18:09 +0200

    Update peripheral_status.c

--- src/widgets/peripheral_status.c
+++ src/widgets/peripheral_status.c
@@ -12,7 +12,6 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/battery.h>
 #include <zmk/display.h>
-#include "status.h"
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/battery_state_changed.h>
