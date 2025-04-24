ef272820ac513e12f6db16376ede41546aa43deb
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-04-24 20:30:02 +0200

    Add files via upload

--- include/nice_view_hid/hid.h
+++ include/nice_view_hid/hid.h
@@ -23,6 +23,17 @@ struct volume_notification {
 
 ZMK_EVENT_DECLARE(volume_notification);
 
+struct media_title_notification {
+    char title[32];
+};
+
+struct media_artist_notification {
+    char artist[32];
+};
+
+ZMK_EVENT_DECLARE(media_title_notification);
+ZMK_EVENT_DECLARE(media_artist_notification);
+
 #ifdef CONFIG_NICE_VIEW_HID_SHOW_LAYOUT
 struct layout_notification {
     uint8_t value;
--- src/widgets/status.h
+++ src/widgets/status.h
@@ -25,7 +25,6 @@ struct zmk_widget_status {
     lv_obj_t *label_artist;
 #endif
 };
-};
 
 int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget);
